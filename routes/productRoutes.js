486c4a8dba09d12859e2d81f3701a1148d1d0842
janusha <janushabimod@gmail.com>
2024-08-30 11:32:44 +0530

    ADD: finished update and delete part of products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,5 +8,7 @@ router.post("/add-sellingprice-date", productController.createInventory);
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
+router.put("/update-inventory/:id", productController.updateInventory);
+
 
 module.exports = router;
